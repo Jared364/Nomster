cb486733fc3bdf786565b7c7dba33ab2b91525d5
Jared364 <jaredand1994@gmail.com>
2019-09-24 22:13:15 +0000

    photos controller/model created and edited, added form to places page for photo uploader setup

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -24,7 +24,6 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
-
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
@@ -57,6 +56,7 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
     @comment = Comment.new
+    @photo = Photo.new
   end
 
   private
