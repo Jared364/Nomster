c293233383158f4fad25cc51706956aa7bbc6f84
Jared364 <jaredand1994@gmail.com>
2019-09-27 21:03:07 +0000

    Added commands to pull commenter and poster info for notification emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,9 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "myemail@email.com", subject: "A comment has been added to your place")
+  def comment_added(comment_added)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email, subject: "A comment has been added to your place")
   end
 end
